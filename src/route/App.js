2c79d99fa38a0c95559d41bf31474b0c906a98da
Zrour Mostfa <moustazro123@gmail.com>
2024-12-05 21:25:28 +0100

    update App.js

--- src/route/App.js
+++ src/route/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "./pages/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
@@ -9,11 +10,9 @@ import ProductList from "../store/ProductList";
 import Container from "../tp1/Container";
 import Form from "../Forms/Form"
 
-import { HashRouter, Routes, Route } from "react-router-dom";
-
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Container />} />
@@ -22,7 +21,6 @@ export default function App() {
           <Route path="*" element={<_404 />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
-
